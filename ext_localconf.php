234f0160c9a4aa3695ae4d7679f9f8b72a716942
Michael Gnessner <technik@epoche-napoleon.net>
2024-02-03 15:12:38 +0100

    [Bugfix] em_localconf.php
    Signed-off-by: Michael Gnessner <technik@epoche-napoleon.net>

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,10 @@
 <?php
 
-	use \Typo3\CMS\Core\Utility\GeneralUtility;
+	declare(strict_types=1);
+
+	use \TYPO3\CMS\Core\Information\Typo3Version;
+	use \TYPO3\CMS\Core\Utility\GeneralUtility;
+	use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 	defined('TYPO3') or die('Access denied.');
 
@@ -9,9 +13,10 @@
 	 */
 	$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['sitepackage'] = 'EXT:sitepackage/Configuration/RTE/Sitepackage.yaml';
 	/***************
-	 * PageTS (if TYPO3 version is below 12, otherwise it is included in Configuration/TsConfig/Page/All.tsconfig)
+	 * PageTS (if TYPO3 version is below 12, otherwise it is included in Configuration/TsConfig/page.tsconfig)
 	 */
 	$versionInformation = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
+	// Import PageTS only if TYPO3 version is below 12 so that it is not included twice
 	if ($versionInformation->getMajorVersion() < 12) {
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:sitepackage/Configuration/TsConfig/Page/All.tsconfig">');
 	}
@@ -21,5 +26,5 @@
 	 */
 	$versionInformation = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
 	if ($versionInformation->getMajorVersion() < 13) {
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:sitepackage/Configuration/user.tsconfig">');
-	}
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('@');
+	};
