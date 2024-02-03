89937cd0f5343e6a497f9c597c7128a921b401a4
Michael Gnessner <technik@epoche-napoleon.net>
2024-02-03 23:43:48 +0100

    [Bugfix] em_localconf.php
    Signed-off-by: Michael Gnessner <technik@epoche-napoleon.net>

--- ext_localconf.php
+++ ext_localconf.php
@@ -15,21 +15,19 @@
 	/***************
 	 * PageTS (if TYPO3 version is below 12, otherwise it is included in Configuration/TsConfig/page.tsconfig)
 	 */
-	/*
+
 	$versionInformation = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
 	// Import PageTS only if TYPO3 version is below 12 so that it is not included twice
 	if ($versionInformation->getMajorVersion() < 12) {
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:sitepackage/Configuration/TsConfig/Page/All.tsconfig">');
 	}
-	*/
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:sitepackage/Configuration/TsConfig/Page/All.tsconfig">');
 
 
 /***************
 	 * Include UserTS
 	 */
-	/*
-	$versionInformation = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
+
+	$versionInformation = \TYPO3\CMS\Composer\Plugin\Core\Utillity\ GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
 	if ($versionInformation->getMajorVersion() < 13) {
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('@');
 	};
