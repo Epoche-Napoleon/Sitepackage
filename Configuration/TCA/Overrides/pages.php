7acbe70b262959fca068a71b5f8a157c7626ca83
Michael Gnessner <technik@epoche-napoleon.net>
2024-02-04 12:59:39 +0100

    [Update] Confioguration Files added
    Signed-off-by: Michael Gnessner <technik@epoche-napoleon.net>

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,27 @@
+<?php
+
+	declare(strict_types=1);
+
+	definned('TYPO3') or die();
+
+	call_user_func(function()
+	{
+		/**
+		 * Temporary variables
+		 */
+		$extensionKey = 'sitepackage';
+
+		/**
+		 * Default PageTS for Sitepackage
+		 */
+
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+			'$extensionKey',
+			'Configuration/TsConfig/Page/All.tsconfig',
+			'EPOCHE NAPOLEON Sitepackage'
+		);
+	});
+	/**
+	* Use doktype and fe_group as default values for new pages
+	 */
+	 $GLOBALS['TCA']['pages']['ctrl']['useColumnsForDefaultValues'] = 'doktype, fe_group';
