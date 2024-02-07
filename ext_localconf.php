dbcb847f75ff9058c66ff1466d084cc736ab12e8
Michael Gnessner <technik@epoche-napoleon.net>
2024-02-08 00:09:47 +0100

    [BUGFIX] ext_localconf.php
    Signed-off-by: Michael Gnessner <technik@epoche-napoleon.net>

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use TYP03\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
