23f61cfde8b840a35210520ecd24dcb7723d909d
Michael Gnessner <technik@epoche-napoleon.net>
2024-02-08 00:11:21 +0100

    [BUGFIX] ext_localconf.php
    Signed-off-by: Michael Gnessner <technik@epoche-napoleon.net>

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,8 +13,6 @@ defined('TYPO3_MODE') || die();
 );
 // Individual RTE Configuration
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['epochenapoleon_preset'] = 'EXT:sitepackage/Configuration/RTE/Yaml/epochenapoleon_preset.yaml';
-
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['EventLinkViewHelper'] = ['epochenapoleon\\sitepackage\\ViewHelpers'];
 $GLOBALS['TCA']['tx_news_domain_model_news']['ctrl']['thumbnail'] = 'fal_media';
 
 $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
