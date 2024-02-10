4842390bcc43a9e6ea2faa5cd5b387f80efdfc79
Michael Gnessner <technik@epoche-napoleon.net>
2024-02-10 16:10:45 +0100

    [Bugfix] Rename RTE-Config.yaml
    Signed-off-by: Michael Gnessner <technik@epoche-napoleon.net>

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,21 +9,17 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 defined('TYPO3_MODE') || die();
 
 // Individual RTE Configuration
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['epochenapoleon_preset'] = 'EXT:sitepackage/Configuration/RTE/Yaml/epochenapoleon_preset.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['epochenapoleon_preset'] = 'EXT:sitepackage/Configuration/RTE/Yaml/Epochenapoleon.yaml';
 $GLOBALS['TCA']['tx_news_domain_model_news']['ctrl']['thumbnail'] = 'fal_media';
 
 $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
 // Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
-/*
 if ($versionInformation->getMajorVersion() < 12) {
 	ExtensionManagementUtility::addPageTSConfig(
 		'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:sitepackage/Configuration/TsConfig/Page.tsconfig">
 	');
 }
-*/
-	ExtensionManagementUtility::addPageTSConfig(
-'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:sitepackage/Configuration/TsConfig/Page.tsconfig">'
-	);
+
 // Adding the default user.tsconfig
 	ExtensionManagementUtility::addUserTSConfig(
 		'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:sitepackage/Configuration/TsConfig/User.tsconfig">
